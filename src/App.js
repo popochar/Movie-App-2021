23d15f118ba4fdaebc9fa075e318172b554507b3
mira <mira509417@gmail.com>
2021-02-07 19:40:45 +0900

    2021-02-07_react 클론코딩 1~4-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ name, picture }) {
+  //console.log(props);
+  return (
+    <div>
+       <h2>I like {name}</h2>
+       <img src={picture}></img> 
+    </div>
+  
+  );
+}
+
+const foodLike = [
+  {
+    name : 'Apple',
+    image : 'https://lh3.googleusercontent.com/proxy/A_EOBWcRjEr_oe7PFiBOJs1aqs5ag1-oB_ev3zD5_tO1gfpQ8U6AZ_NO1s6KLohvoUEnd7JuZ-bXn-Gs4mx1aBYTxG_M-LWZKx04DEbDLI1lzOOo'
+  },
+  {
+    name : 'banana',
+    image : 'https://lh3.googleusercontent.com/proxy/x7GxUxM9-BacgviEtL31Wk-f8h36eO7-pBrqrZ4nPTSXDuvh3QWTazGXQRmqhGNIAZ1AklV0X4OfA2XlI2dbnfXrvzmBXXm4UK1RfBaUaomxuev5QjHQb1wsoyZ33mk12jQN6IDQFoa9SGZsI1LgY3pPWs0'
+  },
+  {
+    name : 'lemon',
+    image : 'https://lh3.googleusercontent.com/proxy/CSmhW2CxxAY9naU_avf_T1VJ1ud7KnlhH8atSox13VX8SpZbrCoF7EANbJuMzsKhkWw5sM20FHrQ-qjdjf8uzRQMi23givAprGlVVtLgihjcCG18O-Z_fz8Cr45jgZGs_f0Ky4JGlzeN'
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello</h1>
+      {
+        foodLike.map(dish =>
+          (
+            <Food name={dish.name} picture={dish.image}></Food>
+          )
+
+
+        )
+
+
+      }
+    </div>   
   );
 }
 
