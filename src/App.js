5826d483fe8be85370aafb23fcc430471faaa3c8
mira <mira509417@gmail.com>
2021-02-14 19:23:39 +0900

    2021-02-14_react 클론코딩 4-3~5-3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+<<<<<<< Updated upstream
 function Food({ name, picture }) {
   //console.log(props);
   return (
@@ -44,6 +45,66 @@ function App() {
       }
     </div>   
   );
+=======
+class App extends React.Component {
+  
+
+
+  state = {
+    count: 0,
+  };
+  
+  add = () => {
+
+    
+   // console.log('add');
+   // this.state.count = 1;
+  
+   this.setState(current => ({ 
+      count: current.count + 1 
+    }));
+  //W  console.log(current);  
+  };
+
+  minus = () => {
+    //console.log('minus');
+    //this.state.count = -1;
+    this.setState(current => ({
+       
+      count: current.count - 1 
+    }));
+  //  console.log(current);
+  };
+  
+
+  render() {
+
+    console.log('render');
+
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    ); 
+  } 
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just compoent update');
+  }
+
+  componentWillUnmount() {
+    console.log('Good bye, my world');
+  }
+
+
+>>>>>>> Stashed changes
 }
+
 
 export default App;
