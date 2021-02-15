80710a3b059b6dcf651ee780137dd13d9015112a
mira <mira509417@gmail.com>
2021-02-15 22:07:04 +0900

    2021-02-15_react 클론코딩 6-2~7-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import axios from 'axios';
+import axios from 'axios'; //http 통신할 때 json형식으로 값 가져올 수 있게 해줌
+import Movie from "./Movie";
 
 class App extends React.Component {
   
@@ -12,28 +13,46 @@ class App extends React.Component {
     const { //ES6에서 사용 구조 분해 할당
       data : {
         data : {movies},
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+      }  //await이 axios
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({ movies , isLoading : false}); //ES6 state와 await 와 같으면 축약형으로 써도 가능
 
   }
 
 
   componentDidMount() {
-   // setTimeout(() => {
-    //  this.setState({ isLoading : false});
-    //}, 6000);
-    
-   // axios.get('https://yts-proxy.now.sh/list_movies.json');
     this.getMovies();   
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
-  }
-
+    return (
+                //className 이유는 JSX이기 때문 (자바스크립트 인지 html 인지 구분 못함)
+      <section className="container">
+        {isLoading  ? ( // 삼항연산자
+          <div className="loader">
+            <span className="loader__text">'Loading...'</span>
+          </div>
+        ) : (
+          <div className="movies">
+          
+         { movies.map((movie) => ( // movie를 map형식으로 가져옴
+            <Movie 
+            key={movie.id} //prop key 값 가져와야되서 넣어줌
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+            />
+          ))}
+          </div>
+        )}
+        </section>
+    );
+  } 
 }
 
 
---
+++ src/Movie.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+function Movie({ title, year, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ui className="movie__genres">
+                {genres.map((genre, index) => { // index는 배열의 원소
+                    return <li key={index} className="movie__genre">{genre}</li>
+                })}
+            </ui>                         
+            <p className="movie__summary">{summary.slice(0, 180)}</p> 
+        </div>
+      </div>
+    );
+}
+
+Movie.prototypes = {
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired, //genres 는 장르가 여러개 이기 때문에 배열로 처리
+
+};
+
+export default Movie;
